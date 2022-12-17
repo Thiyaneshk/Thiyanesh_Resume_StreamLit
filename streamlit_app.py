e8b4ba67b49604c5e17fa650d8a8362eb98f6c2b
Thiyanesh <Thiyaneshk@gmail.com>
2022-12-17 07:06:40 -0400

    Modifications done to resume app -- Added resume

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,10 +12,10 @@ with open('Thiyanesh_PLSQL.pdf', "rb") as pdf_file:
 # --- GENERAL SETTINGS ---
 PAGE_TITLE = "Digital CV | Thiyanesh"
 PAGE_ICON = ":wave:"
-NAME  = "Thiyanesh Kamaraj"
+NAME = "Thiyanesh Kamaraj"
 TITLE = "PLSQL Developer"
-CITY  = "*Halifax*, Nova Scotia"
-MOBILE= "`+1 (782) 882 4409`"
+CITY = "*Halifax*, Nova Scotia"
+MOBILE = "`+1 (782) 882 4409`"
 DESCRIPTION = """
 Oracle PLSQL Developer, Seeking opportunity.
 """
@@ -49,9 +49,9 @@ with col1:
 
 with col2:
     st.write("📫", EMAIL)
-    st.write("☎️",MOBILE)
+    st.write("☎️", MOBILE)
     st.write("⚓", CITY)
-    st.write("🔗",'[LinkedIn](https://www.linkedin.com/in/thiyaneshk)')
+    st.write("🔗", '[LinkedIn](https://www.linkedin.com/in/thiyaneshk)')
     # st.write("🔗",'[GitHub](https://github.com/Thiyaneshk)')
     st.download_button(
         label=" 📄 Download Resume",
@@ -104,7 +104,9 @@ scripting/automation and Python.
 #####################
 # Navigation
 
-st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
+st.markdown(
+    '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">',
+    unsafe_allow_html=True)
 
 st.markdown("""
 <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #16A2CB;">
@@ -134,37 +136,41 @@ st.markdown("""
 </nav>
 """, unsafe_allow_html=True)
 
+
 #####################
 # Custom function for printing text
 def txt(a, b):
-  col1, col2 = st.columns([4,1])
-  with col1:
-    st.markdown(a)
-  with col2:
-    st.markdown(b)
+    col1, col2 = st.columns([4, 1])
+    with col1:
+        st.markdown(a)
+    with col2:
+        st.markdown(b)
+
 
 def txt2(a, b):
-  col1, col2 = st.columns([1,4])
-  with col1:
-    st.markdown(f'`{a}`')
-  with col2:
-    st.markdown(b)
+    col1, col2 = st.columns([1, 4])
+    with col1:
+        st.markdown(f'`{a}`')
+    with col2:
+        st.markdown(b)
+
 
 def txt3(a, b):
-  col1, col2 = st.columns([1,2])
-  with col1:
-    st.markdown(a)
-  with col2:
-    st.markdown(b)
-  
+    col1, col2 = st.columns([1, 2])
+    with col1:
+        st.markdown(a)
+    with col2:
+        st.markdown(b)
+
+
 def txt4(a, b, c):
-  col1, col2, col3 = st.columns([1.5,2,2])
-  with col1:
-    st.markdown(f'`{a}`')
-  with col2:
-    st.markdown(b)
-  with col3:
-    st.markdown(c)
+    col1, col2, col3 = st.columns([1.5, 2, 2])
+    with col1:
+        st.markdown(f'`{a}`')
+    with col2:
+        st.markdown(b)
+    with col3:
+        st.markdown(c)
 
 
 #####################
@@ -200,21 +206,41 @@ st.markdown('''
 #     ''')
 
 
-txt('[Virtusa](https://www.virtusa.com/) , **Senior Consultant**','Chennai, India.')
-txt('10/2018 - 07/2022',' ')
+txt('[Virtusa](https://www.virtusa.com/) , **Senior Consultant**', 'Chennai, India.')
+txt('10/2018 - 07/2022', ' ')
 st.markdown('''
-- Developed database architectural strategies at modeling, design and implementation stages for migration of legacy Sybase database to Oracle database.
-- Migration of MIS application from Sybase to Oracle in view to implement most secure and robust financial reporting application for Citi Bank - Japan.
-- Gathered, defined and refined requirements, led project design and oversaw implementation.
-- Determined areas for improvement and implemented processes which leveraged Oracle security features to alleviate problems.
-- Automated EUC files into automated reports as value-adds to user, which reduced 1hr of human effort per day and reduced cost-to-company of $200,000 per year.
-- Created common utils package for the application with custom functions and autonomous logging, which improved code reusability and code quality.
-- Created scripts in shell/Perl and processes for data loading, integration, maintenance, data quality check and other tools used for the implementation.
-- Agile application development including constant feedback from clients and implementation of robust system.
+- Migration of Citi banks application from Sybase to Oracle in view to
+implement most secure and robust financial reporting application. Gathered, defined and refined requirements, led DB/Application design
+and oversaw implementation with team of 20.
+- Experience with Data flow diagrams, Data dictionary, Database
+normalization with deep understanding in Oracle DB architecture.
+- Automated EUC files into automated reports as value-adds to user, which
+reduced 1hr of human effort per day and reduced cost-to-company of
+$250,000 per year.
+- Created application utility package with custom functions and
+autonomous logging, improving code reusability and code quality.
+- Experience in Oracle supplied packages, Dynamic SQL, Records and
+PL/SQL Tables. Loaded data into Oracle tables using SQL Loader. 
+- Effectively made use of Table, Views, Constraints, Indexes, partitioning, collections, Analytic functions, Materialized views, Query re-write, et al.
+- Expertise in Dynamic SQL, Ref cursors, Collections, exception handling, SQL performance tuning using Cost-Based optimization. 
+- Created Python/Shell scripts for data loading, integration, maintenance, data quality check and used Oracle Migration tool used for migration of
+Sybase Procedure to Oracle Packages/Procedures.
+- Developed/Deployed the solutions to clients in short deadlines
+effectively in Agile in bi-weekly sprint releases.
 ''')
 
-txt('[Oracle Financial Services Software](https://www.oracle.com/) , **Application Developer**','Chennai, India')
-txt('08/2016 - 10/2018',' ')
+
+# - Developed database architectural strategies at modeling, design and implementation stages for migration of legacy Sybase database to Oracle database.
+# - Migration of MIS application from Sybase to Oracle in view to implement most secure and robust financial reporting application for Citi Bank - Japan.
+# - Gathered, defined and refined requirements, led project design and oversaw implementation.
+# - Determined areas for improvement and implemented processes which leveraged Oracle security features to alleviate problems.
+# - Automated EUC files into automated reports as value-adds to user, which reduced 1hr of human effort per day and reduced cost-to-company of $200,000 per year.
+# - Created common utils package for the application with custom functions and autonomous logging, which improved code reusability and code quality.
+# - Created scripts in shell/Perl and processes for data loading, integration, maintenance, data quality check and other tools used for the implementation.
+# - Agile application development including constant feedback from clients and implementation of robust system.
+
+txt('[Oracle Financial Services Software](https://www.oracle.com/) , **Application Developer**', 'Chennai, India')
+txt('08/2016 - 10/2018', ' ')
 st.markdown('''
 - Customization of the application to the needs of the clients, which includes understanding the business requirement, feasibility study, implementation of the same by coding the same which includes creation of database objects like tables, triggers, views and packages, procedures, functions with the highest degree of coding standards. 
 - Catering needs of Flexcube universal banking applications to a range of clients in development and maintenance of clients in the Middle East and Asia Pacific.
@@ -223,8 +249,8 @@ st.markdown('''
 - Implemented system of tracking the bugs spotted in various clients and proactivily fixing the bugs in all the clients running the same version of Flexcube, which decreased number of issues raised clients and inturn improved customer satisfaction.
 ''')
 
-txt('[HCL Technologies Ltd.](https://www.hcltech.com/) , **Software Engineer**','Chennai, India')
-txt('12/2014 - 08/2016',' ')
+txt('[HCL Technologies Ltd.](https://www.hcltech.com/) , **Software Engineer**', 'Chennai, India')
+txt('12/2014 - 08/2016', ' ')
 st.markdown('''
 - Application development with all stages of System Development Life Cycle (SDLC). Applied Database Tuning and Optimization, Normalization under tight schedules of development and implementation. 
 - Development includes Tier upgrade/downgrade/association process where Customers are assigned to tiers according to sales made as per business rules.
@@ -233,8 +259,8 @@ st.markdown('''
 - Created 6 value-adds worth $105,000 which brought new business to the Company and have satisfied the customer as it improved productivity. Obtained high acclaims for the same.
 ''')
 
-txt('[C-Edge Tech.](https://cedge.in/) ( A TCS - SBI Enterprise ) , **Developer**','Chennai, India')
-txt('10/2012 - 12/2014',' ')
+txt('[C-Edge Tech.](https://cedge.in/) ( A TCS - SBI Enterprise ) , **Developer**', 'Chennai, India')
+txt('10/2012 - 12/2014', ' ')
 st.markdown('''
 - Managing a Center of `10` professors, OOOOO least `20+` research publications annually. 
 - B@NCS - Core banking implementation for over `25` Banks which contained over 850+ Banks in them. Worked in Client location which involved direct interaction with clients to obtain the requirement.
@@ -262,14 +288,13 @@ txt3('SDLC', '`Agile`, `Waterfall`')
 
 txt3('Data ETL', '`SQL*Loader`, `External Table`, `DataPump`')
 
-
 #####################
 st.markdown('''
 ## Education
 ''')
 
 txt('**B.Tech** (Information Technology), *Anna University*, India',
-'2002-2006')
+    '2002-2006')
 st.markdown('''
 - GPA: `7.61`
 - Bachelors of Technology, Information Technology.
@@ -291,7 +316,6 @@ txt2('GitHub', 'https://github.com/Thiyaneshk')
 # txt2('Publons', 'https://publons.com/a/303133/')
 
 
-
 #####################
 st.markdown('''
 ## Contact me
@@ -300,9 +324,9 @@ txt2('Email', 'thiyaneshk@gmail.com')
 txt2('Mobile', '+1 (782) 882 4409')
 
 with open("dp.jpg", "rb") as file:
-  btn = st.download_button(
-    label="Download Resume",
-    data=file,
-    file_name="Thiyanesh_PLSQL.pdf",
-    mime="application/pdf"
-  )
+    btn = st.download_button(
+        label="Download Resume",
+        data=file,
+        file_name="Thiyanesh_PLSQL.pdf",
+        mime="application/pdf"
+    )
